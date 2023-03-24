2436061cd07c3954ee3728cbe836f354c627b7ae
nguyenvanlong1309 <108638225+nguyenvanlong1309@users.noreply.github.com>
2023-03-24 19:57:55 +0700

    fix

--- initialize.php
+++ initialize.php
@@ -5,6 +5,6 @@ if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"123456");
 if(!defined('DB_NAME')) define('DB_NAME',"traffic_offense_db");
 ?>
