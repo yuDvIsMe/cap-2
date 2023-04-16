36f6cf6f5e17e41a7c53d632539c8bd72c550c39
nguyenvanlong1309 <108638225+nguyenvanlong1309@users.noreply.github.com>
2023-04-16 17:45:05 +0700

    Upload

--- initialize.php
+++ initialize.php
@@ -5,5 +5,5 @@ if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"123456");
-if(!defined('DB_NAME')) define('DB_NAME',"traffic_offense_db");
+if(!defined('DB_NAME')) define('DB_NAME',"tvms");
 ?>
