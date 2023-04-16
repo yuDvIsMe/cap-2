9f0c5777c4613b90d0ea3f96aca5291893dc95e8
nguyenvanlong1309 <108638225+nguyenvanlong1309@users.noreply.github.com>
2023-04-16 22:09:03 +0700

    Upload

--- home.php
+++ home.php
@@ -1,56 +1,58 @@
-<div class="d-none d-md-block">
-    <div class="container-fluid">
-        <div class="row" style = "display: flex; align-items: center;">
-            <div class="col-md-8">
-                <div class="top-bar-left">
-                    <div class="text">
-                        <a href="<?php echo base_url ?>" class="home-title"><h1>TVMS</h1></a>
-                        <a href="<?php echo base_url ?>" class="home-title"><h2>Cổng dịch vụ công thành phố</h2></a>
+<div>
+    <div class="d-none d-md-block">
+        <div class="container-fluid">
+            <div class="row" style = "display: flex; align-items: center;">
+                <div class="col-md-8">
+                    <div class="top-bar-left">
+                        <div class="text">
+                            <a href="<?php echo base_url ?>" class="home-title"><h1>TVMS</h1></a>
+                            <a href="<?php echo base_url ?>" class="home-title"><h2>Cổng dịch vụ công thành phố</h2></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="top-bar-right">
-                    <div class="social-login">
-                        <a class="btn-login" href="<?php echo base_url . 'admin' ?>">
-                            Đăng nhập
-                        </a>
-                        <a class="btn-login" href="<?php echo base_url ?>?page=sign-up">
-                            Đăng ký
-                        </a>
+                <div class="col-md-4">
+                    <div class="top-bar-right">
+                        <div class="social-login">
+                            <a class="btn-login" href="<?php echo base_url . 'admin' ?>">
+                                Đăng nhập
+                            </a>
+                            <a class="btn-login" href="<?php echo base_url ?>?page=sign-up">
+                                Đăng ký
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="single">
-    <div style="margin-top: 40px;">
-        <div class="row">
-            <div class="col-md-12">
+    
+
+    <div class="single">
+        <div style="margin-top: 40px;">
+            <div class="display-flex">
                 <div class="fact single-item-home" data-aos="fade-up" data-aos-duration="2000">
                     <div class="container">
-                        <div class="row align-items-center">
-                            <a class="col-lg-3 col-md-6 " style="cursor: pointer;" href="<?php echo base_url ?>?page=about" >
+                        <div class="display-flex">
+                            <a class="col-lg-3" style="cursor: pointer;" href="<?php echo base_url ?>?page=about" >
                                 <div class="fact-item" >
                                     <i class="fas fa-user icon-home"></i>
                                     <h2>Giới thiệu</h2>
                                 </div>
                             </a>
-                            <a class="col-lg-3 col-md-6" style="cursor: pointer;" href="<?php echo base_url ?>?page=news">
+                            <a class="col-lg-3" style="cursor: pointer;" href="<?php echo base_url ?>?page=news">
                                 <div class="fact-item">
                                     <i class="fas fa-newspaper icon-home"></i>
                                     <h2>Tin tức</h2>
                                 </div>
                             </a>
-                            <a class="col-lg-3 col-md-6" style="cursor: pointer;" href="<?php echo base_url ?>?page=information-lookup">
+                            <a class="col-lg-3" style="cursor: pointer;" href="<?php echo base_url ?>?page=information-lookup">
                                 <div class="fact-item">
                                     <i class="fas fa-credit-card icon-home"></i>
                                     <h2>Tra cứu thông tin</h2>
                                 </div>
                             </a>
-                            <a class="col-lg-3 col-md-6" style="cursor: pointer;" href="<?php echo base_url ?>?page=contact">
+                            <a class="col-lg-3" style="cursor: pointer;" href="<?php echo base_url ?>?page=contact">
                                 <div class="fact-item">
                                     <i class="fas fa-address-card icon-home"></i>
                                     <h2>Liên hệ</h2>
@@ -59,7 +61,42 @@
                         </div>
                     </div>
                 </div>
-                <img class="img-home" src="img/trangchu.png" alt="Image">
+            </div>
+            
+            <img class="img-home" src="img/trangchu.png" alt="Image">
+        </div>
+    </div>
+    <div class="testimonial">
+        <div class="container">
+            <div class="section-header">
+                <!-- <p>Testimonial Carousel</p>
+                <h2>100% Positive Customer Reviews</h2> -->
+            </div>
+            <div class="owl-carousel testimonials-carousel">
+                <div class="testimonial-item-news">
+                    <img class="img-testimonial-item-news" src="img/news-2.png" alt="Image">
+                    <a href="news-detail.html">
+                        Khởi tố "cặp tình nhân" diễn xiếc trên đèo hải vân
+                    </a>
+                </div>
+                <div class="testimonial-item-news">
+                    <img class="img-testimonial-item-news" src="img/news-3.png" alt="Image">
+                    <a href="news-detail.html">
+                        CSGT Đắk Nông bắt giữ đối tượng tàng trữ chất ma tuý
+                    </a>
+                </div>
+                <div class="testimonial-item-news">
+                    <img class="img-testimonial-item-news" src="img/news-4.png" alt="Image">
+                    <a href="news-detail.html">
+                        Xử lý vi phạm nồng độ cồn để người dân "tâm phục"
+                    </a>
+                </div>
+                <div class="testimonial-item-news">
+                    <img class="img-testimonial-item-news" src="img/news-1.png" alt="Image">
+                    <a href="news-detail.html">
+                        Sử dụng “đăng kiểm giả” có thể bị khởi tố hình sự
+                    </a>
+                </div>
             </div>
         </div>
     </div>
