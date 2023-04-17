d5c21fbd3d235c5171514505cfa4a87a3c31d4b9
Vinhduy1204 <vinhduy2201@gmail.com>
2023-04-17 11:44:54 +0700

    update db for news function

--- home.php
+++ home.php
@@ -1,12 +1,16 @@
 <div>
     <div class="d-none d-md-block">
         <div class="container-fluid">
-            <div class="row" style = "display: flex; align-items: center;">
+            <div class="row" style="display: flex; align-items: center;">
                 <div class="col-md-8">
                     <div class="top-bar-left">
                         <div class="text">
-                            <a href="<?php echo base_url ?>" class="home-title"><h1>TVMS</h1></a>
-                            <a href="<?php echo base_url ?>" class="home-title"><h2>Cổng dịch vụ công thành phố</h2></a>
+                            <a href="<?php echo base_url ?>" class="home-title">
+                                <h1>TVMS</h1>
+                            </a>
+                            <a href="<?php echo base_url ?>" class="home-title">
+                                <h2>Cổng dịch vụ công thành phố</h2>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -26,7 +30,7 @@
         </div>
     </div>
 
-    
+
 
     <div class="single">
         <div style="margin-top: 40px;">
@@ -34,8 +38,8 @@
                 <div class="fact single-item-home" data-aos="fade-up" data-aos-duration="2000">
                     <div class="container">
                         <div class="display-flex">
-                            <a class="col-lg-3" style="cursor: pointer;" href="<?php echo base_url ?>?page=about" >
-                                <div class="fact-item" >
+                            <a class="col-lg-3" style="cursor: pointer;" href="<?php echo base_url ?>?page=about">
+                                <div class="fact-item">
                                     <i class="fas fa-user icon-home"></i>
                                     <h2>Giới thiệu</h2>
                                 </div>
@@ -62,41 +66,26 @@
                     </div>
                 </div>
             </div>
-            
+
             <img class="img-home" src="img/trangchu.png" alt="Image">
         </div>
     </div>
     <div class="testimonial">
         <div class="container">
             <div class="section-header">
-                <!-- <p>Testimonial Carousel</p>
-                <h2>100% Positive Customer Reviews</h2> -->
             </div>
             <div class="owl-carousel testimonials-carousel">
-                <div class="testimonial-item-news">
-                    <img class="img-testimonial-item-news" src="img/news-2.png" alt="Image">
-                    <a href="news-detail.html">
-                        Khởi tố "cặp tình nhân" diễn xiếc trên đèo hải vân
-                    </a>
-                </div>
-                <div class="testimonial-item-news">
-                    <img class="img-testimonial-item-news" src="img/news-3.png" alt="Image">
-                    <a href="news-detail.html">
-                        CSGT Đắk Nông bắt giữ đối tượng tàng trữ chất ma tuý
-                    </a>
-                </div>
-                <div class="testimonial-item-news">
-                    <img class="img-testimonial-item-news" src="img/news-4.png" alt="Image">
-                    <a href="news-detail.html">
-                        Xử lý vi phạm nồng độ cồn để người dân "tâm phục"
-                    </a>
-                </div>
-                <div class="testimonial-item-news">
-                    <img class="img-testimonial-item-news" src="img/news-1.png" alt="Image">
-                    <a href="news-detail.html">
-                        Sử dụng “đăng kiểm giả” có thể bị khởi tố hình sự
-                    </a>
-                </div>
+                <?php
+                $qry = $conn->query("SELECT * from `news_list` order by unix_timestamp(post_date) desc ");
+                while ($row = $qry->fetch_assoc()) :
+                ?>
+                    <div class="testimonial-item-news">
+                        <img class="img-testimonial-item-news" src="<?php echo $row['image'] ?>" alt="Image">
+                        <a href="news-detail.html">
+                            <?php echo $row['title'] ?>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
             </div>
         </div>
     </div>
--- news.php
+++ news.php
@@ -5,30 +5,33 @@
     <div class="container">
         <ul class="breadcrumb">
             <li class="item-breadcrumb">
-                <a  href="<?php echo base_url ?>?page=home">Trang chủ</a>
+                <a href="<?php echo base_url ?>?page=home">Trang chủ</a>
                 <a style="padding-left: 10px">></a>
             </li>
             <li class="item-breadcrumb">
-                <a  href="<?php echo base_url ?>?page=news">Tin tức</a>
+                <a href="<?php echo base_url ?>?page=news">Tin tức</a>
             </li>
         </ul>
         <div class="row">
             <div class="col-md-6">
-                <img src="img/news-1.png" alt="" style="width: 100%; height: 100%;">
-                <p style="font-size: 24px; color: #000;">
-                    Sử dụng “đăng kiểm giả” có thể bị khởi tố hình sự
-                </p>
+                <?php
+                $qry = $conn->query("SELECT * from `news_list` order by unix_timestamp(post_date) desc limit 1 ");
+                while ($row = $qry->fetch_assoc()) :
+                ?>
+                    <img src="<?php echo $row['image'] ?>" alt="" style="width: 100%; height: 100%;">
+                    <a href="#" style="font-size: 24px; color: #000;">
+                        <?php echo $row['title'] ?>
+                    </a>
+                <?php endwhile; ?>
             </div>
             <div class="col-md-6" style="display: flex; height: 100%; flex-direction: column;">
-                <a href="news-detail.html" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Sử dụng “giấy đăng kiểm giả” có thể bị khởi tố hình sự </a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Sơ kết phong trào thi đua “Thanh niên CAND học tập, thực hiện 06 điều Bác Hồ dạy” </a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Đắk Nông: Tuyên truyền ATGT và trao tặng công trình "Thắp sáng học đường"</a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Đẩy mạnh công tác tuyên truyền bảo đảm TTATGT trong trường học Xử lý nghiêm xe chở mía quá khổ, quá tải</a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Thiếu tá Cảnh sát giao thông 23 lần hiến máu cứu người</a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Lái ô tô chạy quá tốc độ, chống đối CSGT bị khởi tố</a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> CSGT TP Hồ Chí Minh chặn đứng hai vụ tụ tập đua xe trái phép</a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Quyên góp ủng hộ bệnh nhân nghèo điều trị tại viện Bệnh viện Đa khoa Đức Giang</a>
-                <a href="" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> Đẩy mạnh tuyên truyền tại các cơ sở giáo dục ở Khánh Hòa </a>
+                <?php
+                $qry = $conn->query("SELECT * from `news_list` order by unix_timestamp(post_date) desc ");
+                while ($row = $qry->fetch_assoc()) :
+                ?>
+                    <a href="#" style="padding-bottom: 15px"><i class="fab fa-telegram-plane"></i> <?php echo $row['title'] ?>
+                    </a>
+                <?php endwhile; ?>
             </div>
         </div>
     </div>
