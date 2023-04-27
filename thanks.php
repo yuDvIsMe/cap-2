046697790bd83b82118d46e7db335dae066946de
Vinhduy1204 <vinhduy2201@gmail.com>
2023-04-27 13:02:57 +0700

    update db vnpay

--- thanks.php
+++ thanks.php
@@ -1,12 +1,44 @@
-<?php require_once('inc/header.php') ?>
-<?php require_once('inc/subnav.php') ?>
-<!-- Contact Start -->
+<?php 
+require_once('./config.php');
+require_once('./classes/DBConnection.php');
+require_once('inc/header.php');
+require_once('inc/subnav.php');
+
+if(isset($_GET['vnp_Amount'])){
+    $vnp_Amount= $_GET['vnp_Amount'];
+    $vnp_BankCode= $_GET['vnp_BankCode'];
+    $vnp_BankTranNo= $_GET['vnp_BankTranNo'];
+    $vnp_CardType= $_GET['vnp_CardType'];
+    $vnp_OrderInfo= $_GET['vnp_OrderInfo'];
+    $vnp_PayDate= $_GET['vnp_PayDate'];
+    $vnp_TmnCode= $_GET['vnp_TmnCode'];
+    $vnp_TransactionNo= $_GET['vnp_TransactionNo'];
+    $ticket_no = $_SESSION['ticket_no'];
+
+
+    $insert_vnpay = "INSERT INTO vnpay_payment(vnp_Amount, ticket_no, vnp_BankCode, vnp_BankTranNo, vnp_CardType, vnp_OrderInfo, vnp_PayDate, vnp_TmnCode, vnp_TransactionNo) 
+                     VALUE('".$vnp_Amount."','".$ticket_no."','".$vnp_BankCode."','".$vnp_BankTranNo."','".$vnp_CardType."','".$vnp_OrderInfo."','".$vnp_PayDate."','".$vnp_TmnCode."',
+                     '".$vnp_TransactionNo."')";
+    $query = $conn->query($insert_vnpay);
+    // mysqli_query($mysqli, $insert_vnpay);
+    if($query){
+        echo '<h3>Giao dich VNPAY thanh cong</h3>';
+    }else{
+        echo '<h3>Giao dich VNPAY that bai</h3>';
+
+    }
+}
+
+
+
+?>
+
+
 <div class="contact mt-125">
     <div class="container">
-        <h1>Cảm ơn</h1>
+        <h1>Trang cảm ơn</h1>
         
     </div>
 
 </div>
-<!-- Contact End -->
-<?php require_once('inc/subnav.php') ?>
+ 
