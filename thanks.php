c1f78fed066239d142778a590e57727c9df4ddbb
Vinhduy1204 <vinhduy2201@gmail.com>
2023-04-28 12:38:42 +0700

    update db payment

--- thanks.php
+++ thanks.php
@@ -13,19 +13,25 @@ if(isset($_GET['vnp_Amount'])){
     $vnp_PayDate= $_GET['vnp_PayDate'];
     $vnp_TmnCode= $_GET['vnp_TmnCode'];
     $vnp_TransactionNo= $_GET['vnp_TransactionNo'];
+    $vnp_ResponseCode= $_GET['vnp_ResponseCode'];
     $ticket_no = $_SESSION['ticket_no'];
 
 
-    $insert_vnpay = "INSERT INTO vnpay_payment(vnp_Amount, ticket_no, vnp_BankCode, vnp_BankTranNo, vnp_CardType, vnp_OrderInfo, vnp_PayDate, vnp_TmnCode, vnp_TransactionNo) 
+    $insert_vnpay = "INSERT INTO vnpay_payment(vnp_Amount, ticket_no, vnp_BankCode, vnp_BankTranNo, vnp_CardType, vnp_OrderInfo, vnp_PayDate, vnp_TmnCode, vnp_TransactionNo,vnp_ResponseCode) 
                      VALUE('".$vnp_Amount."','".$ticket_no."','".$vnp_BankCode."','".$vnp_BankTranNo."','".$vnp_CardType."','".$vnp_OrderInfo."','".$vnp_PayDate."','".$vnp_TmnCode."',
-                     '".$vnp_TransactionNo."')";
+                     '".$vnp_TransactionNo."','".$vnp_ResponseCode."')";
     $query = $conn->query($insert_vnpay);
-    // mysqli_query($mysqli, $insert_vnpay);
     if($query){
         echo '<h3>Giao dich VNPAY thanh cong</h3>';
     }else{
         echo '<h3>Giao dich VNPAY that bai</h3>';
-
+    }
+    
+}
+if ($_GET['vnp_ResponseCode'] == '00') {
+    $update_qry = $conn->query("UPDATE `violation_list` SET `status` = 1 WHERE `ticket_no` = '{$ticket_no}'");
+    if (!$update_qry) {
+        echo $conn->error;
     }
 }
 
@@ -37,7 +43,6 @@ if(isset($_GET['vnp_Amount'])){
 <div class="contact mt-125">
     <div class="container">
         <h1>Trang cảm ơn</h1>
-        
     </div>
 </div>
  
