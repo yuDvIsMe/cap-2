8910b81c4bf545c87159040659f65f2e386498d6
nguyenvanlong1309 <108638225+nguyenvanlong1309@users.noreply.github.com>
2023-05-17 13:56:53 +0700

    Upload

--- admin/login.php
+++ admin/login.php
@@ -8,68 +8,239 @@
      background-size:cover;
      background-repeat:no-repeat;
    }
+   /* Login */
+
+.login-box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 400px;
+    padding: 40px;
+    transform: translate(-50%, -50%);
+    background: rgba(0,0,0,.5);
+    box-sizing: border-box;
+    box-shadow: 0 15px 25px rgba(0,0,0,.6);
+    border-radius: 10px;
+  }
+  
+  .login-h2 {
+    margin: 0 0 30px;
+    padding: 0;
+    color: #fff;
+    text-align: center;
+  }
+  
+  .login-box .user-box {
+    position: relative;
+  }
+  
+  .login-input {
+    width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+  }
+  .login-label {
+    position: absolute;
+    top:0;
+    left: 0;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    pointer-events: none;
+    transition: .5s;
+  }
+  
+  .login-box .user-box input:focus ~ .login-label,
+  .login-box .user-box input:valid ~ .login-label {
+    top: -20px;
+    left: 0;
+    color: #03e9f4;
+    font-size: 12px;
+  }
+  
+  .login-submit {
+    position: relative;
+    display: inline-block;
+    padding: 10px 20px;
+    color: #03e9f4;
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    overflow: hidden;
+    transition: .5s;
+    margin-top: 40px;
+    letter-spacing: 4px
+  }
+  .login-submit:hover {
+    background: #03e9f4;
+    color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 5px #03e9f4,
+                0 0 25px #03e9f4,
+                0 0 50px #03e9f4,
+                0 0 100px #03e9f4;
+  }
+  
+  .login-span {
+    position: absolute;
+    display: block;
+  }
+  
+  .login-box a span:nth-child(1) {
+    top: 0;
+    left: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(90deg, transparent, #03e9f4);
+    animation: btn-anim1 1s linear infinite;
+  }
+  
+  @keyframes btn-anim1 {
+    0% {
+      left: -100%;
+    }
+    50%,100% {
+      left: 100%;
+    }
+  }
+  
+  .login-box a span:nth-child(2) {
+    top: -100%;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(180deg, transparent, #03e9f4);
+    animation: btn-anim2 1s linear infinite;
+    animation-delay: .25s
+  }
+  
+  @keyframes btn-anim2 {
+    0% {
+      top: -100%;
+    }
+    50%,100% {
+      top: 100%;
+    }
+  }
+  
+  .login-box a span:nth-child(3) {
+    bottom: 0;
+    right: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(270deg, transparent, #03e9f4);
+    animation: btn-anim3 1s linear infinite;
+    animation-delay: .5s
+  }
+  
+  @keyframes btn-anim3 {
+    0% {
+      right: -100%;
+    }
+    50%,100% {
+      right: 100%;
+    }
+  }
+  
+  .login-box a span:nth-child(4) {
+    bottom: -100%;
+    left: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(360deg, transparent, #03e9f4);
+    animation: btn-anim4 1s linear infinite;
+    animation-delay: .75s
+  }
+  
+  @keyframes btn-anim4 {
+    0% {
+      bottom: -100%;
+    }
+    50%,100% {
+      bottom: 100%;
+    }
+  }
+  
  </style>
 <body class="hold-transition login-page ">
   <script>
     start_loader()
   </script>
-  <h2 class="text-center pb-4 mb-4 text-light"><?php echo $_settings->info('name') ?> - Admin Đăng nhập</h2>
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-primary">
-    <div class="card-body">
-      <form id="login-frm" action="" method="post">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" name="username" placeholder="Tài khoản">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+  <div class="login-box">
+    <h2 class="text-center pb-4 mb-4 text-light">Admin Đăng nhập</h2>
+    <div class="card card-primary">
+      <div class="card-body">
+        <form id="login-frm" action="" method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" name="username" placeholder="Tài khoản">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Mật khẩu">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" name="password" placeholder="Mật khẩu">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row justify-content-between">
-          <div class="col">
-            <a href="<?php echo base_url ?>">Trang chủ</a>
+          <div class="row justify-content-between">
+            <div class="col">
+              <a href="<?php echo base_url ?>">Trang chủ</a>
+            </div>
+            <div class="col text-right">
+              <button type="submit" class="btn btn-primary btn-flat btn-sm">Đăng nhập</button>
+            </div>
           </div>
-          <!-- /.col -->
-          <div class="col text-right">
-            <button type="submit" class="btn btn-primary btn-flat btn-sm">Đăng nhập</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <!-- /.social-auth-links -->
-
-      <!-- <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p> -->
-      
+        </form>
+      </div>
     </div>
-    <!-- /.card-body -->
   </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
+  <!-- /.login-box -->
+  <!-- <div>
+    <div class="login-box">
+      <h2 class="login-h2">Đăng nhập</h2>
+      <form  method="post">
+          <div class="user-box">
+              <input class="login-input" type="text" name="" required="">
+              <label class="login-label">Tên người dùng <span style="color: red">*</span></label>
+          </div>
+          <div class="user-box">
+              <input class="login-input" type="password" name="" required="">
+              <label class="login-label">Mật khẩu <span style="color: red">*</span></label>
+          </div>
+          <h5>Bạn chưa có tài khoản? Hãy <a href="sign-up.html">đăng ký</a></h5>
+          <button class="login-submit" type="submit">
+              <span class="login-span"></span>
+              <span class="login-span"></span>
+              <span class="login-span"></span>
+              <span class="login-span"></span>
+              Đăng nhập
+          </button>
+      </form>
+    </div>
+  </div> -->
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
 
-<script>
-  $(document).ready(function(){
-    end_loader();
-  })
-</script>
+  <script>
+    $(document).ready(function(){
+      end_loader();
+    })
+  </script>
 </body>
 </html>
